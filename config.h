238d2ff3503683f1fe839aa8e52871e3e593820a
Michael <yagodarov.misha.2003@mail.ru>
2022-09-11 22:53:53 +0300

    feat: added config

---
+++ config.h
@@ -0,0 +1,15 @@
+#ifndef GAME_CONFIG_H
+#define GAME_CONFIG_H
+
+
+// control settings
+#define MOVE_TOP 'W'
+#define MOVE_BOTTOM 'S'
+#define MOVE_RIGHT 'D'
+#define MOVE_LEFT 'A'
+
+// update time in microseconds
+#define UPDATE_TIME_MCSEC 3000
+
+
+#endif //GAME_CONFIG_H
