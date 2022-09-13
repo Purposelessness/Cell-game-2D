53b9dd3becc34613e5bdc33e617290d54c52fe0c
Michael <yagodarov.misha.2003@mail.ru>
2022-09-13 15:57:22 +0300

    test(TestField): new tests
    - Copy operator
    - Move constructor
    - Move operator

--- gtests/TestField.cpp
+++ gtests/TestField.cpp
@@ -72,3 +72,101 @@ TEST_F(TestField, CopyConstructor) {
     f3.setCellEvent(0, 0, byeEvent);
     EXPECT_NE(f1.getCell(0, 0), f3.getCell(0, 0));
 }
+
+TEST_F(TestField, CopyOperator) {
+    int w, h;
+    f1 = Field{-10, 0};
+    Field f4{};
+    f4 = f1;
+    f4.getSize(w, h);
+    EXPECT_EQ(w, 0);
+    EXPECT_EQ(w, 0);
+
+    f1 = Field{-100, -100};
+    Field f5{};
+    f5 = f1;
+    f5.getSize(w, h);
+    EXPECT_EQ(w, 0);
+    EXPECT_EQ(w, 0);
+
+    f1 = Field{25, 35};
+    f1.setCellEvent(0, 0, helloEvent);
+
+    Field f3{};
+    f3 = f1;
+    f3.getSize(w, h);
+    EXPECT_EQ(w, 25);
+    EXPECT_EQ(h, 35);
+    EXPECT_EQ(f1.getCell(0, 0), f3.getCell(0, 0));
+    f3.setCellEvent(0, 0, byeEvent);
+    EXPECT_NE(f1.getCell(0, 0), f3.getCell(0, 0));
+}
+
+TEST_F(TestField, MoveConstructor) {
+    int w1, h1;
+    int w2, h2;
+
+    f1 = Field{30, 40};
+    f1.setCellEvent(0, 0, helloEvent);
+    Field f3{std::move(f1)};
+
+    f1.getSize(w1, h1);
+    f3.getSize(w2, h2);
+    EXPECT_EQ(w1, 0);
+    EXPECT_EQ(h1, 0);
+    EXPECT_EQ(w2, 30);
+    EXPECT_EQ(h2, 40);
+    EXPECT_EQ(f1.getCell(0, 0), Cell{});
+    Cell c{};
+    c.changeEvent(helloEvent);
+    EXPECT_EQ(f3.getCell(0, 0), c);
+
+    f1 = Field{-20, 40};
+    f1.setCellEvent(0, 0, helloEvent);
+    Field f4{std::move(f1)};
+
+    f1.getSize(w1, h1);
+    f4.getSize(w2, h2);
+    EXPECT_EQ(w1, 0);
+    EXPECT_EQ(h1, 0);
+    EXPECT_EQ(w2, 0);
+    EXPECT_EQ(h2, 0);
+    EXPECT_EQ(f1.getCell(0, 0), Cell{});
+    EXPECT_NE(f4.getCell(0, 0), c);
+}
+
+TEST_F(TestField, MoveOperator) {
+    int w1, h1;
+    int w2, h2;
+
+    f1 = Field{30, 40};
+    f1.setCellEvent(0, 0, helloEvent);
+    Field f3{};
+    f3 = std::move(f1);
+
+    f1.getSize(w1, h1);
+    f3.getSize(w2, h2);
+    EXPECT_EQ(w1, 0);
+    EXPECT_EQ(h1, 0);
+    EXPECT_EQ(w2, 30);
+    EXPECT_EQ(h2, 40);
+    EXPECT_EQ(f1.getCell(0, 0), Cell{});
+    Cell c{};
+    c.changeEvent(helloEvent);
+    EXPECT_EQ(f3.getCell(0, 0), c);
+
+
+    f1 = Field{-20, 40};
+    f1.setCellEvent(0, 0, helloEvent);
+    Field f4{};
+    f4 = std::move(f1);
+
+    f1.getSize(w1, h1);
+    f4.getSize(w2, h2);
+    EXPECT_EQ(w1, 0);
+    EXPECT_EQ(h1, 0);
+    EXPECT_EQ(w2, 0);
+    EXPECT_EQ(h2, 0);
+    EXPECT_EQ(f1.getCell(0, 0), Cell{});
+    EXPECT_NE(f4.getCell(0, 0), c);
+}
