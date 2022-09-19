f1434cbb53a08ff241c8f171330a808b604e5ab2
Michael <yagodarov.misha.2003@mail.ru>
2022-09-19 13:27:51 +0300

    feat(utility): new type-helper structs
    -remove const and reference
    - check if variadic templates are same

--- Utility/Template.h
+++ Utility/Template.h
@@ -5,6 +5,7 @@
 
 #include <type_traits>
 #include <functional>
+#include <tuple>
 
 template<template<typename...> class base, typename derived>
 struct is_base_of_template_impl {
@@ -22,6 +23,27 @@ template<typename T, typename... U>
 long getAddress(std::function<T(U...)> func) {
     return *reinterpret_cast<long*>(reinterpret_cast<char*>(&func));
 }
+
+template<typename T>
+using remove_cv_reference = std::remove_cv_t<std::remove_reference_t<T>>;
+
+template<typename T, typename U>
+using is_same = std::is_same<remove_cv_reference<T>, remove_cv_reference<U>>;
+
+namespace detail {
+    template<bool...>
+    struct bool_pack;
+    template<bool... bs>
+    using all_true = std::is_same<bool_pack<bs..., true>, bool_pack<true, bs...>>;
+}
+template<typename... Ts>
+using all_true = detail::all_true<Ts::value...>;
+
+template<typename TTuple, typename UTuple>
+struct are_same;
+
+template<typename... Ts, typename... Us>
+struct are_same<std::tuple<Ts...>, std::tuple<Us...>> : all_true<is_same<Ts, Us>...> {};
 
 #endif //GAME_UTILITY_TEMPLATE_H_
 #pragma clang diagnostic pop
