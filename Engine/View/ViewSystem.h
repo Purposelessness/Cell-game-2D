e050624475d581ea8182189a9a082d61800800a7
Michael <yagodarov.misha.2003@mail.ru>
2022-10-01 00:03:42 +0300

    refactor(engine/view): changed way of initializing renderers
    - now has methods to add and remove renderers with ability to call constructor with arguments specified
    - tuple now holds unique_ptr -> no more memory leaks

--- Engine/View/ViewSystem.h
+++ Engine/View/ViewSystem.h
@@ -1,28 +1,48 @@
 #ifndef GAME_ENGINE_VIEW_VIEWSYSTEM_H_
 #define GAME_ENGINE_VIEW_VIEWSYSTEM_H_
 
+#include <memory>
 #include <vector>
 #include <tuple>
 
 #include "../../Utility/Tuple.h"
+#include "../../Utility/Template.h"
 #include "ViewRenderer.h"
 
-template<TViewRenderer... Args>
+template<TViewRenderer... Ts>
 class ViewSystem {
+    using Types = std::tuple<Ts...>;
+
 public:
-    explicit ViewSystem(Args*... args) {
-        _types = std::make_tuple(args...);
+    ViewSystem() {
+
+    }
+
+    template<TViewRenderer T, typename... Args>
+    requires (has_type<T, Types>::value)
+    void addRenderer(Args&&... args) {
+        auto& ptr = std::get<std::unique_ptr<T>>(_renderers);
+        if (!ptr.get())
+            ptr = std::make_unique<T>(std::forward<Args>(args)...);
+    }
+
+    template<TViewRenderer T>
+    requires (has_type<T, Types>::value)
+    void removeRenderer() {
+        std::get<std::unique_ptr<T>>(_renderers).reset();
     }
 
     template<TViewMessage T>
     void render(T&& message) {
-        Tuple::forEach(_types, [message=std::forward<T>(message)](TViewRenderer auto* t) {
+        Tuple::forEach(_renderers, [message=std::forward<T>(message)](auto& t) -> void {
+            if (t.get() == nullptr)
+                return;
             t->update(message);
         });
     }
 
 private:
-    std::tuple<Args*...> _types;
+    std::tuple<std::unique_ptr<Ts>...> _renderers;
 };
 
 #endif //GAME_ENGINE_VIEW_VIEWSYSTEM_H_
