ae7a6a1b521aca184a0406e54d80578284ad73af
Michael <yagodarov.misha.2003@mail.ru>
2022-10-09 17:35:02 +0300

    feat(utility): di container

---
+++ Utility/DI/Container.h
@@ -0,0 +1,52 @@
+#ifndef GAME_UTILITY_DI_CONTAINER_H_
+#define GAME_UTILITY_DI_CONTAINER_H_
+
+#include <tuple>
+
+#include "Helper.h"
+#include "../Template.h"
+#include "../Tuple.h"
+
+namespace di {
+
+    template<typename... TServices>
+    class Container {
+        using Types = std::tuple<TServices...>;
+        using SharedPtrTuple = shared_ptr_tuple<Types>;
+        SharedPtrTuple _services;
+
+    public:
+        Container() = default;
+
+        explicit Container(std::shared_ptr<TServices...> services) {
+            _services = std::make_tuple(services);
+        }
+
+        template<typename T, typename... Args>
+        void addService(Args... args) {
+            std::get<std::shared_ptr<T>>(_services) = std::make_shared<T>(std::forward<Args>(args)...);
+        }
+
+        template<TInjectClient T, typename... Args>
+        std::shared_ptr<T> create(Args&&... args) {
+            using MethodArgs = typename extract_method_arguments<decltype(&T::inject)>::args_tuple;
+            static_assert(has_all_type<Types, MethodArgs>::value);
+
+            auto obj = std::make_shared<T>(std::forward<Args>(args)...);
+            MethodArgs services_to_inject;
+            Tuple::forEach(services_to_inject, [services=_services](auto& service) {
+                service = std::get<std::decay_t<decltype(service)>>(services);
+            });
+
+            auto inject_lambda = [&obj]<typename... InjectArgs>(InjectArgs&&... args) {
+                obj->inject(std::move(args)...);
+            };
+            std::apply(inject_lambda, std::move(services_to_inject));
+            return obj;
+        }
+    };
+
+} // di
+
+
+#endif //GAME_UTILITY_DI_CONTAINER_H_
