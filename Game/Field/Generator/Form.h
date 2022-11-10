19ce62bf1aae960cee531c94726296e01fe90dcd
Michael Yagodarov <purposelessness@yandex.ru>
2022-11-10 18:28:38 +0300

    fix: fixes in rect form and grid placer

--- Game/Field/Generator/Form.h
+++ Game/Field/Generator/Form.h
@@ -5,31 +5,67 @@
 #include "FieldGenerator.h"
 
 template <typename T, typename Changer>
-concept TForm = requires { TCellChanger<Changer>; } and
-                requires(T t) {
-                  t(std::declval<const Changer&>(), std::declval<FieldChangerInput&>());
-                };
+concept TForm =
+    requires { TCellChanger<Changer>; } and
+    requires(T t) {
+      t(std::declval<const Changer&>(), std::declval<FieldChangerInput&>());
+    };
 
 namespace form {
 
+template <int X, int Y, bool Centered = true>
+class Rect;
+
 template <int X, int Y>
-class Rect {
+class Rect<X, Y, true> {
  public:
   template <TCellChanger T>
   void operator()(const T& changer, FieldChangerInput& data) {
+    auto width = static_cast<int>(data.cells[0].size());
+    auto height = static_cast<int>(data.cells.size());
+    auto position = Point{data.position.x - X / 2, data.position.y - Y / 2};
+    if (position.x < 0) {
+      position.x = 0;
+    }
+    if (position.y < 0) {
+      position.y = 0;
+    }
+
     for (auto j = 0; j < Y; ++j) {
       for (auto i = 0; i < X; ++i) {
+        if (position.x + i >= width || position.y + j >= height) {
+          continue;
+        }
+        changer(data.cells[position.y + j][position.x + i]);
+      }
+    }
+  }
+};
+
+template <int X, int Y>
+class Rect<X, Y, false> {
+ public:
+  template <TCellChanger T>
+  void operator()(const T& changer, FieldChangerInput& data) {
+    auto width = static_cast<int>(data.cells[0].size());
+    auto height = static_cast<int>(data.cells.size());
+
+    for (auto j = 0; j < Y; ++j) {
+      for (auto i = 0; i < X; ++i) {
+        if (data.position.x + i >= width || data.position.y + j >= height) {
+          continue;
+        }
         changer(data.cells[data.position.y + j][data.position.x + i]);
       }
     }
   }
 };
 
-template <int Length>
-class VerticalLine : Rect<1, Length> {};
+template <int Length, bool Centered = true>
+class VerticalLine : public Rect<1, Length, Centered> {};
 
-template <int Length>
-class HorizontalLine : Rect<Length, 1> {};
+template <int Length, bool Centered = true>
+class HorizontalLine : public Rect<Length, 1, Centered> {};
 
 }  // namespace form
 
--- Game/Field/Generator/GridPlacer.h
+++ Game/Field/Generator/GridPlacer.h
@@ -1,6 +1,7 @@
 #ifndef GAME_GAME_FIELD_GENERATOR_GRIDPLACER_H_
 #define GAME_GAME_FIELD_GENERATOR_GRIDPLACER_H_
 
+#include <cmath>
 #include <utility>
 
 #include "Concepts.h"
@@ -14,19 +15,25 @@ class GridPlacer {
       : _changer(std::move(changer)) {}
 
   void operator()(std::vector<std::vector<Cell>>& cells) {
-    auto height = static_cast<int>(cells.size());
-    auto y_space = (height - YCount) / (YCount + 1);
-    y_space = y_space == 0 ? 1 : y_space;
-    auto width = static_cast<int>(cells[0].size());
-    auto x_space = (width - XCount) / (XCount + 1);
-    x_space = x_space == 0 ? 1 : x_space;
+    auto height = static_cast<float>(cells.size());
+    // auto y_space = (height - YCount) / (YCount + 1);
+    auto y_space = height / (YCount + 1);
+    auto width = static_cast<float>(cells[0].size());
+    // auto x_space = (width - XCount) / (XCount + 1);
+    auto x_space = width / (XCount + 1);
     FieldChangerInput input{cells, Point{0, 0}};
-    for (auto y = y_space; y < height; y += y_space) {
-      input.position.y = y;
-      for (auto x = x_space; x < width; x += x_space) {
-        input.position.x = x;
+    float y = y_space;
+    for (int y_i = 0; y_i < YCount; ++y_i) {
+      auto new_y = static_cast<int>(std::round(y));
+      input.position.y = new_y >= height ? new_y - 1 : new_y;
+      float x = x_space;
+      for (int x_i = 0; x_i < XCount; ++x_i) {
+        auto new_x = static_cast<int>(std::round(x));
+        input.position.x = new_x >= width ? new_x - 1 : new_x;
         _form(*_changer, input);
+        x += x_space;
       }
+      y += y_space;
     }
   }
 
